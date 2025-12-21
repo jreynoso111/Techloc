c0cb03ed619b1353742a03ba4d594204fff7082e
jreynoso111 <jreynoso111@gmail.com>
2025-12-21 11:52:34 -0500

    Use shared supabase module client

---
+++ assets/scripts/config.js
@@ -0,0 +1,27 @@
+export const SUPABASE_URL = 'https://ewgtclzscwbokxmzxbcu.supabase.co';
+export const SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3Z3RjbHpzY3dib2t4bXp4YmN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwODA3MzIsImV4cCI6MjA4MDY1NjczMn0.QkM72rVeBpm6uGgBVdG4ulIzEg3V_7T8usqvIf6vBto';
+
+export const VEHICLES_COLUMNS = new Set([
+  'deal status',
+  'customer id',
+  'unit type',
+  'model year',
+  'model',
+  'shortvin',
+  'inv. prep. stat.',
+  'phys_loc',
+  'deal completion',
+  'gps fix',
+  'gps fix reason',
+  'pt status',
+  'pt serial',
+  'encore serial',
+  'moving',
+  'pt last read',
+  'state loc',
+  'pt city',
+  'pt zipcode',
+  'lat',
+  'long',
+]);
