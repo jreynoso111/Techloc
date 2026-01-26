10aea362177ea0c5cbe7b062080b65123c1ca98e
jreynoso111 <jreynoso111@gmail.com>
2026-01-25 19:53:45 -0500

    Relax blacklist deal status for repo items

--- assets/scripts/core/state.js
+++ assets/scripts/core/state.js
@@ -14,6 +14,7 @@ export const DashboardState = {
     unitTypeSelection: [],
     vehicleStatusKey: '',
     vehicleStatusSelection: [],
+    gpsOfflineOnly: false,
   },
   vehiclesRaw: new Map(),
   schema: [],
